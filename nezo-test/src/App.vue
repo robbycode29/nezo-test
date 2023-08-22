5456e50ea389f3564c2743979fd3a26c94b4b670
Robert <robertandrei.oprescu@gmail.com>
2023-08-22 17:11:46 +0300

    added breadcrumbs

--- nezo-test/src/App.vue
+++ nezo-test/src/App.vue
@@ -1,40 +1,42 @@
 <template>
-  <div class="w-screen h-screen flex">
+  <div class="flex w-screen h-screen">
     <!-- <div class="w-280px h-3/4 bg-222222"></div> -->
-    <div class="w-full h-76px shadow-md flex items-center">
-      <div class="mx-24px my-16px w-full flex justify-between">
-        <div class="flex w-400px border rounded-lg h-44px items-center">
-          <input class="outline-none flex-1 ml-3" type="text" placeholder="Search"/>
-          <img src="@/assets/header/magnifier-glass.svg" class="w-5 h-5 mr-3"/>
-        </div>
-        <div class="flex-1"></div>
-        <div class="w-396px flex items-center gap-8">
-          <button class="h-6 w-6 p-1 bg-EBEBEB rounded-full flex items-center justify-center transform transition duration-500 hover:bg-007FFF">
-            <img src="@/assets/header/plus.svg" class=""/>
-          </button>
-          <button class="h-6 w-6 flex items-center justify-center group">
-            <img src="@/assets/header/setting-2.svg" class="group-hover:animate-spin-fast"/>
-          </button>
-          <button class="h-7 w-34px relative group">
-            <img src="@/assets/header/notification.svg" class="h-6 w-6 hover:animate-ring group-hover:animate-ring"/>
-            <div class="p-1 h-11px bg-FA4B4B rounded-full absolute top-0 left-3 flex items-center">
-              <span class="text-10px text-white">23</span>
-            </div>
-          </button>
-          <button class="h-44px w-210px flex items-center relative justify-center gap-4 group">
-            <div class="relative">
-              <img src="@/assets/header/profile.svg" class="z-10 relative"/>
-            </div>
-            <div class="flex flex-col flex-1 relative">
-              <h3 class="text-base font-semibold self-start">Madalina Popescu</h3>
-              <span class="text-xs self-start">Super Admin</span>
-            </div>
-            <div class="bg-007FFF z-0 absolute right-0 w-full scale-0 rounded-lg h-full opacity-0 group-hover:animate-grow-profile"></div>
-          </button>
+    <div class="flex flex-col items-center w-screen h-screen">
+      <div class="w-full h-76px shadow-md flex items-center">
+        <div class="mx-24px my-16px w-full flex justify-between">
+          <div class="flex w-400px border rounded-lg h-44px items-center">
+            <input class="outline-none flex-1 ml-3" type="text" placeholder="Search"/>
+            <img src="@/assets/header/magnifier-glass.svg" class="w-5 h-5 mr-3"/>
+          </div>
+          <div class="flex-1"></div>
+          <div class="w-396px flex items-center gap-8">
+            <button class="h-6 w-6 p-1 bg-EBEBEB rounded-full flex items-center justify-center transform transition duration-500 hover:bg-007FFF">
+              <img src="@/assets/header/plus.svg" class=""/>
+            </button>
+            <button class="h-6 w-6 flex items-center justify-center group">
+              <img src="@/assets/header/setting-2.svg" class="group-hover:animate-spin-fast"/>
+            </button>
+            <button class="h-7 w-34px relative group">
+              <img src="@/assets/header/notification.svg" class="h-6 w-6 hover:animate-ring group-hover:animate-ring"/>
+              <div class="p-1 h-11px bg-FA4B4B rounded-full absolute top-0 left-3 flex items-center">
+                <span class="text-10px text-white">23</span>
+              </div>
+            </button>
+            <button class="h-44px w-210px flex items-center relative justify-center gap-4 group">
+              <div class="relative">
+                <img src="@/assets/header/profile.svg" class="z-10 relative"/>
+              </div>
+              <div class="flex flex-col flex-1 relative">
+                <h3 class="text-base font-semibold self-start">Madalina Popescu</h3>
+                <span class="text-xs self-start">Super Admin</span>
+              </div>
+              <div class="bg-007FFF z-0 absolute right-0 w-full scale-0 rounded-lg h-full opacity-0 group-hover:animate-grow-profile"></div>
+            </button>
+          </div>
         </div>
       </div>
+      <router-view/>
     </div>
-    <router-view/>
   </div>
 </template>
 
--- nezo-test/tailwind.config.js
+++ nezo-test/tailwind.config.js
@@ -12,11 +12,15 @@ module.exports = {
         '13px': '13px',
         '210px': '210px',
         '44px': '44px',
+        '190px': '190px',
+        '18px': '18px',
       },
       height: {
         '76px': '76px',
         '44px': '44px',
         '11px': '11px',
+        '58px': '58px',
+        '18px': '18px',
       },
       colors: {
         '222222': '#222222',
@@ -27,14 +31,19 @@ module.exports = {
       margin: {
         '24px': '24px',
         '16px': '16px',
+        '100px': '100px',
+      },
+      padding: {
+        '100px': '100px',
       },
       fontSize: {
         '10px': '10px',
+        '12px': '12px',
       },
       animation: {
         'ring': 'swing 0.5s ease-in-out 2',
         'spin-fast': 'spin 1s 1',
-        'grow-profile': 'growprofile 0.5s 1'
+        'grow-profile': 'growprofile 0.3s 1'
       },
       keyframes: {
         swing: {
